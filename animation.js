ccbcd9c42c877a140db217f16c99cc2dc7ef20e3
Riffat Akash <riffatakash@gmail.com>
2021-04-19 15:30:54 +0600

    Add files via upload

--- animation.js
+++ animation.js
@@ -1,5 +1,14 @@
+var displayTitle = document.querySelector('.patch_title img');
 var displayCounter = document.querySelector('.patch_countdown');
 
+var sehriSet = [
+    "3:59","3:59","3:58","3:57","3:55",
+    "3:54","3:53","3:52","3:51","3:50",
+    "3:50","3:49","3:49","3:48","4:15",
+    "4:14","4:13","4:12","4:11","4:10","4:09",
+    "4:08","4:07","4:06","4:05","4:05","4:04",
+    "4:03","4:02","4:01","4:00"
+];
 
 var iftarSet = [
     "18:31","18:31","18:31","18:32",
@@ -10,7 +19,6 @@ var iftarSet = [
     "18:28","18:28","18:29","18:29","18:29",
     "18:30","18:30"
 ];
- 
 
 setInterval(function () {
     var today = new Date();
@@ -18,9 +26,19 @@ setInterval(function () {
     var curTime = today.getHours()*60*60+ today.getMinutes()*60+today.getSeconds();
 
     var checkIftar = iftarSet[curDate].split(":");
+    var checkSehri = sehriSet[curDate].split(":");
     var timeIftar = checkIftar[0]*60*60 + checkIftar[1]*60;
-    checkIftarTime(curDate, curTime);
+    var timeSehri = checkSehri[0]*60*60 + checkSehri[1]*60;
 
+    if (timeSehri<timeIftar && timeSehri>curTime && timeSehri>=0){
+        checkSehriTime(curDate, curTime);
+    }
+    else if (timeSehri<timeIftar && timeIftar<curTime) {
+        checkSehriTime(curDate, curTime);
+    }
+    else {
+        checkIftarTime(curDate, curTime);
+    }
 },1e3);
 
 
@@ -29,15 +47,26 @@ function checkIftarTime(todayDate, curTime) {
     var setTime = time[0]*60*60 + time[1]*60;
     var diffTime = setTime - curTime;
     if (diffTime<setTime && diffTime>=0){
+        displayTitle.src = "assets/iftar.png";
         displayCounter.innerHTML = printTimer(diffTime);
-    } 
-    else {
-        var lastTime = setTime+24*60*60;
-        var iftariEnd = lastTime - curTime;
-        displayCounter.innerHTML = printTimer(iftariEnd);
     }
 }
 
+function checkSehriTime(todayDate, curTime) {
+    var time = sehriSet[todayDate].split(":");
+    var setTime = time[0]*60*60 + time[1]*60;
+    var diffTime = setTime - curTime;
+
+    if (diffTime<setTime && diffTime>=0){
+        displayTitle.src = "assets/sehri.png";
+        displayCounter.innerHTML = printTimer(diffTime);
+    } else {
+        var lastTime = setTime+24*60*60;
+        var sehriEnd = lastTime - curTime;
+        displayTitle.src = "assets/sehri.png";
+        displayCounter.innerHTML = printTimer(sehriEnd);
+    }
+}
 
 function printTimer(sec) {
     hr = Math.floor(sec / 3600) % 24;
